1e337f224ba8b5e57b52e8f0468e2c6671f9bc5c
vaibhav8119 <vaibhav8119@gmail.com>
2020-09-25 20:12:38 +0530

    2nd push

--- student-attendance/src/main/java/com/example/sms/StudentAttendanceApplication.java
+++ student-attendance/src/main/java/com/example/sms/StudentAttendanceApplication.java
@@ -1,13 +1,19 @@
 package com.example.sms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentAttendanceApplication {
+public class StudentAttendanceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentAttendanceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello");
 	}
 
 }
